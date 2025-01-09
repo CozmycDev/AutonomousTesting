70bb06127204240efe08cc655a51207940ea0087
cozmyc <172064888+CozmycDev@users.noreply.github.com>
2025-01-09 15:51:31 -0500

    initial commit

---
+++ cogs/PingCommand.py
@@ -0,0 +1,30 @@
+import discord
+
+from discord import option
+from discord.commands import slash_command
+from discord.ext import commands
+
+
+class PingCommand(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @slash_command(name='ping', description='Example slash command.')
+    @option(
+        "test_string",
+        str,
+        description="",
+        required=True
+    )
+    async def test_command(
+        self,
+        ctx: discord.ApplicationContext,
+        test_string: str
+    ):
+        await ctx.defer()
+        await ctx.respond(content=f"you said {test_string}")
+        
+        
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(PingCommand(bot))
